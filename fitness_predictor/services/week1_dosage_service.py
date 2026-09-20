3c83a6dec4fc360a59eea7081190dc3a571ceaa2
Naitik kedia <kedianaitik2006@gmail.com>
2026-09-21 01:11:17 +0530

    Add dosage service diagnostics

--- fitness_predictor/services/week1_dosage_service.py
+++ fitness_predictor/services/week1_dosage_service.py
@@ -7,11 +7,21 @@ from model_downloader import ensure_models_available
 
 class Week1DosageService:
     def __init__(self, predictor=None):
+        print("[DOSAGE] Constructor START", flush=True)
+
         if predictor is None:
+            print("[DOSAGE] Calling ensure_models_available()", flush=True)
             ensure_models_available()
+            print("[DOSAGE] Models available", flush=True)
+
+            print("[DOSAGE] Creating MLDosagePredictor", flush=True)
             self.predictor = MLDosagePredictor()
+            print("[DOSAGE] MLDosagePredictor CREATED", flush=True)
         else:
             self.predictor = predictor
+            print("[DOSAGE] Existing predictor supplied", flush=True)
+
+        print("[DOSAGE] Constructor END", flush=True)
 
     def generate(
         self,
